2c755be9afa43bb90b6ba16d88ff25a22748dd74
Sergio Donato Camacho do Nascimento <sergiodcdn@gmail.com>
2023-07-03 16:18:12 -0300

    seção sobre e habilidades, att mediquerie

--- static/script.js
+++ static/script.js
@@ -3,10 +3,25 @@
 const menuMobile = document.querySelector('.menu-mobile')
 const body = document.querySelector('body')
 
+
 menuMobile.addEventListener('click', () => {
     menuMobile.classList.contains("bi-list")
         ? menuMobile.classList.replace("bi-list", "bi-x")
         : menuMobile.classList.replace("bi-x", "bi-list");
 
     body.classList.toggle("menu-nav-active")
+})
+
+
+/* fecha o menu ao clicar em algum item e mostra hamburguinho */
+
+const navItem = document.querySelectorAll('.nav-item')
+
+navItem.forEach(item => {
+    item.addEventListener("click", () => {
+        if (body.classList.contains("menu-nav-active")) {
+            body.classList.remove("menu-nav-active")
+            menuMobile.classList.replace("bi-x", "bi-list")
+        }
+    })
 })
