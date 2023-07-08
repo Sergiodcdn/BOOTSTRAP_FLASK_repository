6e2b3cf58879b7d8a219b14f5009df4feed6ae9c
Sergio Donato Camacho do Nascimento <sergiodcdn@gmail.com>
2023-07-08 10:45:35 -0300

    up email service

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
 from flask import Flask, render_template, redirect
+from flask_mail import Mail, Message
+from config import email, senha
 
 app = Flask(__name__)
+app.secret_key = 'senha'
+
+mail_settings = {
+    'MAIL_SERVER': 'smtp.gmail.com',
+    'MAIL_PORT': 465,
+    'MAIL_USEL_TLS': False,
+    'MAIL_USE_SSL': True,
+    'MAIL_USERNAME': email,
+    'MAIL_PASSWORD': senha
+}
 
 @app.route('/')
 def index():
