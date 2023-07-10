e06bf19b3cc96ca9991c91dcb77cf51d877a3b56
Sergio Donato Camacho do Nascimento <sergiodcdn@gmail.com>
2023-07-10 16:21:41 -0300

    main end

--- app.py
+++ app.py
@@ -1,30 +1,8 @@
-from flask import Flask, render_template, redirect, request, flash
+from flask import Flask, render_template
 from flask_mail import Mail, Message
-from config import email, senha
+
 
 app = Flask(__name__)
-app.secret_key = 'senha'
-
-mail_settings = {
-    'MAIL_SERVER': 'smtp.gmail.com',
-    'MAIL_PORT': 465,
-    'MAIL_USEL_TLS': False,
-    'MAIL_USE_SSL': True,
-    'MAIL_USERNAME': email,
-    'MAIL_PASSWORD': senha
-}
-
-
-app.config.update(mail_settings)
-
-mail = Mail(app)
-
-
-class Contato:
-    def __init__(self, nome, email, mensagem):
-        self.nome = nome,
-        self, email = email,
-        self.mensagem = mensagem
 
 
 @app.route('/')
@@ -32,34 +10,7 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/send', methods=['GET', 'POST'])
-def send():
-    if request.method == 'POST':
-        formContato = Contato(
-            request.form["nome"],
-            request.form['email'],
-            request.form['mensagem']
-        )
-
-        msg = Message(
-            subject=f'{formContato.nome} enviou msg no Portfólio',
-            sender=app.config.get('MAIL_USERNAME'),
-            recipients=['sergiodcdn@gmail.com',
-                        app.config.get('MAIL_USERNAME')],
-            body=f'''
-
-                {formContato.nome} com o e-mail {formContato.email}, te enviou a seguinte msg:
-
-                {formContato.mensagem}
-            '''
-        )
-
-        mail.send(msg)
-
-        flash('Msg enviada com sucesso!')
-
-    return redirect('/')
-
-
 if __name__ == '__main__':
     app.run(debug=True)
+
+
